ae65d00688be16b3c81d22dea6b5dfcd19c368fe
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2023-07-17 23:25:14 +0530

    Add heading, input and button in form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState, FormEvent } from "react";
+
 export default function Home() {
+  const [inputVal, setInputVal] = useState("");
+  const { push } = useRouter();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    push(`/prediction/${inputVal}`);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello world</div>
-    </main>
+    <div>
+      <div>
+        <h1>Enter Your Name</h1>
+      </div>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="text-black"
+          placeholder="Type your name..."
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
+        <button type="submit">Predict Data</button>
+      </form>
+    </div>
   );
 }
