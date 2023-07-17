6bf66fba2ddf2bd938d2a985dc02d42bc046aa8b
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2023-07-17 23:50:33 +0530

    Add function to call respective api

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,3 +1,39 @@
-export default function Page({ params }: any) {
-  return <div>{params.name} </div>;
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io/?name=${name}`);
+  return res.json();
+};
+const getPredictedGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io/?name=${name}`);
+  return res.json();
+};
+const getPredictedCountry = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io/?name=${name}`);
+  return res.json();
+};
+
+interface Params {
+  params: { name: string };
+}
+
+export default async function Page({ params }: Params) {
+  const ageData = getPredictedAge(params.name);
+  const genderData = getPredictedGender(params.name);
+  const countryData = getPredictedCountry(params.name);
+
+  const [age, gender, country] = await Promise.all([
+    ageData,
+    genderData,
+    countryData,
+  ]);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className=" text-5xl my-8">Personal Info</div>
+      <div className="text-center text-2xl">
+        <div className="my-5">Name: {params.name}</div>
+        <div>Age: {age?.age}</div>
+        <div>Gender: {gender?.gender}</div>
+        <div>Country: {country?.country[0]?.country_id}</div>
+      </div>
+    </div>
+  );
 }
